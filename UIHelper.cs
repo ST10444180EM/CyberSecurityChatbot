d2321bf9ea4f33b29fb2c184404c5874fece373b
ST10444180EM <ST10444180@myemeris.edu.za>
2026-03-29 23:36:37 +0200

    Implemented ASCII UI and header

---
+++ UIHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace CyberSecurityChatbot
+{
+    internal class UIHelper
+    {
+            public static void ShowHeader()
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("=========================================");
+                Console.WriteLine("   CYBERSECURITY AWARENESS BOT");
+                Console.WriteLine("=========================================");
+
+                Console.WriteLine(@"
+       (^_^)
+      <(   )>
+       /   \
+");
+
+                Console.ResetColor();
+            }
+
+            public static void TypeText(string message)
+            {
+                foreach (char c in message)
+                {
+                    Console.Write(c);
+                    Thread.Sleep(30);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
