707c42ddc43d1bc5dda58e134d333df1c2b8d2b7
ST10444180EM <ST10444180@myemeris.edu.za>
2026-03-29 23:13:06 +0200

    Commit 2: Added audio

---
+++ AudioHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Media;
+
+namespace CyberSecurityChatbot
+{
+    internal class AudioHelper
+    {
+        public static void PlayGreeting()
+        {
+            try
+            {
+                SoundPlayer player = new SoundPlayer("welcome.wav");
+                player.PlaySync();
+            }
+            catch
+            {
+                Console.WriteLine("Audio could not be played.");
+            }
+        }
+    }
+}
