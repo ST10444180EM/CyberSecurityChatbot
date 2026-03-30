fedf934388fd26b60640abf75e8f6dced18b143c
ST10444180EM <ST10444180@myemeris.edu.za>
2026-03-30 22:20:58 +0200

    Final UI improvements and code cleanup

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 {
     internal class Program
     {
-       
+        static void Main(string[] args)
+        {
+            //Header 
+            Console.Title = "Cybersecurity Awareness Bot";
+
+            UIHelper.ShowHeader();
+            AudioHelper.PlayGreeting();
+
+            Chatbot bot = new Chatbot();
+            bot.Start();
+        }
     }
 }
